132886f9eaf825a59c0ec1748a7ef460847e1819
Ivaylo Getov <contact@ivaylogetov.com>
2014-04-30 18:15:46 -0400

    More work on nav

--- js/axion.js
+++ js/axion.js
@@ -1,3 +1,17 @@
+var s = skrollr.init({
+    forceHeight: false,
+    smoothScrolling: false
+});
+
+$("#topNav a").click(function( event ) {
+    event.preventDefault();
+    var d = $(this).attr('href');
+    if (d=="#info") {
+        s.animateTo(s.relativeToAbsolute(document.getElementById('info'), 'top', 'top'), {duration: 1500, easing: "swing"});
+    }
+});
+
+
 $(document).ready(function () {
 
     var headSize = $('.header').height();
