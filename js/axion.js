9e48308f0be9cabd6936f502f9c9a08fdc67e3ff
Ivaylo Getov <contact@ivaylogetov.com>
2014-05-01 02:28:00 -0400

    javascript updates
    Moved everything into $(document).ready(), got rid of status bar for
    nav links.

--- js/axion.js
+++ js/axion.js
@@ -1,68 +1,86 @@
-var s = skrollr.init({
-    forceHeight: false,
-    smoothScrolling: false,
-    constants: {
-        blogTop: function() {
-            return this.relativeToAbsolute(document.getElementById('blog'), 'top', 'top');
-        },
-        blogBottom: function() {
-            return this.relativeToAbsolute(document.getElementById('blog'), 'top', 'bottom');
-        },
-        blogHeight: function() {
-            var t = this.relativeToAbsolute(document.getElementById('blog'), 'top', 'top');
-            var b = this.relativeToAbsolute(document.getElementById('blog'), 'top', 'bottom');
-            console.log(b);
-            return b - t;
-        }
-    },
-    easing: {
-        easeInQuad: function (p) { return p*p },
-        easeOutQuad: function (p) { return p*(2-p) },
-        easeInOutQuad: function (p) { return p<.5 ? 2*p*p : -1+(4-2*p)*p },
-        easeInCubic: function (p) { return p*p*p },
-        easeOutCubic: function (p) { return (--p)*p*p+1 },
-        easeInOutCubic: function (p) { return p<.5 ? 4*p*p*p : (p-1)*(2*p-2)*(2*p-2)+1 },
-        easeInQuart: function (p) { return p*p*p*p },
-        easeOutQuart: function (p) { return 1-(--p)*p*p*p },
-        easeInOutQuart: function (p) { return p<.5 ? 8*p*p*p*p : 1-8*(--p)*p*p*p },
-        easeInQuint: function (p) { return p*p*p*p*p },
-        easeOutQuint: function (p) { return 1+(--p)*p*p*p*p },
-        easeInOutQuint: function (p) { return p<.5 ? 16*p*p*p*p*p : 1+16*(--p)*p*p*p*p }
-    }
-});
-
-$("#topNav a").click(function( event ) {
-    event.preventDefault();
-    var d = $(this).attr('href');
-    if (d=="#info") {
-        s.animateTo(s.relativeToAbsolute(document.getElementById('info'), 'top', 'top'), {duration: 1500, easing: "easeInOutQuart"});
-    } else if (d=="#who") {
-        s.animateTo(s.relativeToAbsolute(document.getElementById('who'), 'top', 'top'), {duration: 3000, easing: "easeInOutQuart"});
-    } else if (d=="#contact") {
-        s.animateTo(s.relativeToAbsolute(document.getElementById('contact'), 'top', 'top'), {duration: 4500, easing: "easeInOutQuart"});
-    }
-});
-
-
 $(document).ready(function () {
 
-    var headSize = $('.header').height();
-    var whichPic = 1;
 
+    // we still have to implement preload for the hallway!
 
+    //var headSize = $('.header').height();
+
+    // start skrollr
+    var s = skrollr.init({
+        forceHeight: false,
+        smoothScrolling: false,
+        constants: {
+            blogTop: function() {
+                return this.relativeToAbsolute(document.getElementById('blog'), 'top', 'top');
+            },
+            blogBottom: function() {
+                return this.relativeToAbsolute(document.getElementById('blog'), 'top', 'bottom');
+            },
+            blogHeight: function() {
+                var t = this.relativeToAbsolute(document.getElementById('blog'), 'top', 'top');
+                var b = this.relativeToAbsolute(document.getElementById('blog'), 'top', 'bottom');
+                //console.log(b);
+                return b - t;
+            }
+        },
+        easing: {
+            easeInQuad: function (p) { return p*p },
+            easeOutQuad: function (p) { return p*(2-p) },
+            easeInOutQuad: function (p) { return p<.5 ? 2*p*p : -1+(4-2*p)*p },
+            easeInCubic: function (p) { return p*p*p },
+            easeOutCubic: function (p) { return (--p)*p*p+1 },
+            easeInOutCubic: function (p) { return p<.5 ? 4*p*p*p : (p-1)*(2*p-2)*(2*p-2)+1 },
+            easeInQuart: function (p) { return p*p*p*p },
+            easeOutQuart: function (p) { return 1-(--p)*p*p*p },
+            easeInOutQuart: function (p) { return p<.5 ? 8*p*p*p*p : 1-8*(--p)*p*p*p },
+            easeInQuint: function (p) { return p*p*p*p*p },
+            easeOutQuint: function (p) { return 1+(--p)*p*p*p*p },
+            easeInOutQuint: function (p) { return p<.5 ? 16*p*p*p*p*p : 1+16*(--p)*p*p*p*p }
+        }
+    });
+
+    /*
     $('#nav').affix({
       offset: {
         top: headSize
       }
-    }); 
-    
+    });
+    */
+    // keep track of which hallway image is active
+    var whichPic = 1;
 
+    // once video is loaded and playing, get rid of poster frame to eliminate stutter on loop
     $(".vidCover").on("play", function(){
         $(this).attr("poster","");
     });
 
-    $(window).scroll(function(){
+    // get rid of status bar for the initial navigation links (becasue they're so close to the bottom it gets in the way)
+    $("#topNav a").each(function() {
+        $(this).removeAttr("href");
+        $(this).css("cursor","pointer");
+    });
 
+    // animate scrolling for navigation links
+    $("#topNav a").click(function( event ) {
+        event.preventDefault();
+        var scrollTime = ($("#blog").outerHeight() / $("#info").outerHeight()) + 4500;
+        //var d = $(this).attr('href');
+        var d = "#" + $(this).attr('data-target');
+        //console.log(d);
+
+        if (d=="#info") {
+            s.animateTo(s.relativeToAbsolute(document.getElementById('info'), 'top', 'top'), {duration: 1500, easing: "easeInOutQuart"});
+        } else if (d=="#who") {
+            s.animateTo(s.relativeToAbsolute(document.getElementById('who'), 'top', 'top'), {duration: 3000, easing: "easeInOutQuart"});
+        } else if (d=="#blog") {
+            s.animateTo(s.relativeToAbsolute(document.getElementById('blog'), 'top', 'top'), {duration: 4500, easing: "easeInOutQuart"});
+        } else if (d=="#contact") {
+            s.animateTo(s.relativeToAbsolute(document.getElementById('contact'), 'top', 'top'), {duration: scrollTime, easing: "easeInOutQuart"});
+        }
+    }); 
+
+    // This is the hallway animation
+    $(window).scroll(function(){
         var posFromTop = $(window).scrollTop() - s.relativeToAbsolute(document.getElementById('blog'), 'bottom', 'top');
         var totHeight = s.relativeToAbsolute(document.getElementById('blog'), 'top', 'bottom') - s.relativeToAbsolute(document.getElementById('blog'), 'bottom', 'top');
 
@@ -85,13 +103,10 @@ $(document).ready(function () {
 
             //var picSrc = "url(img/bgseq/" + picNo + ".jpg)";
             //$('.slideB').css("background-image",picSrc);
+
+            //console.log(picSrc);
+            //console.log(picNo);
         }
-        
-        
-        
-        
-        //console.log(picSrc);
-        //console.log(picNo);
     });
 
 });
